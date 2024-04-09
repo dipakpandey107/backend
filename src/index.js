aac7f8fd210f263ff53062e243ff9546197d338b
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-04-09 22:54:43 +0530

    express init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// require('dotenv').config({ path: './.env' });
-
 import dotenv from "dotenv";
 import connectDB from "./db/index.js"
 
@@ -7,8 +5,17 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Database connection failed",err);
+})
 
+// require('dotenv').config({ path: './.env' });
 /*
 const app = express();
 
